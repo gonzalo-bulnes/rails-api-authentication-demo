11bc07016718fd5530e6fced4287864fd9cb9273
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2015-02-24 12:24:32 -0300

    Add HomeController, enable token authentication
    Now authenticating through token authentication is possible,
    but is not mandatory to acceed to the '/' content.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   acts_as_token_authentication_handler_for User, fallback_to_devise: false
 
   def index
-    render json: { current_user: current_user.try(:email), message: 'You are successfully authenticated!' }
+    message = current_user.presence ? 'You are successfully authenticated!' : 'No authentication is required.'
+    render json: { current_user: current_user.try(:email), message: message }
   end
 end
