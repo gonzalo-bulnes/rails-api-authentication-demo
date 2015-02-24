d40fa7fd5c94528cd65bace87577ca790e6e215c
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2015-02-24 12:27:43 -0300

    Add HomeController#restricted_index which requires authentication
    Authentication is now required. Token authentication can be used
    by users to authenticate themselves.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,21 @@
 class HomeController < ApplicationController
   acts_as_token_authentication_handler_for User, fallback_to_devise: false
+  before_action :require_authentication!, only: :restricted_index
 
   def index
     message = current_user.presence ? 'You are successfully authenticated!' : 'No authentication is required.'
     render json: { current_user: current_user.try(:email), message: message }
   end
+
+  def restricted_index
+    require_authentication!
+    message = current_user.presence ? 'You are successfully authenticated!' : 'If you see this message, authentication is not required.'
+    render json: { current_user: current_user.try(:email), message: message }
+  end
+
+  private
+
+    def require_authentication!
+      throw(:warden, scope: :user) unless current_user.presence
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  get '/restricted_index' => 'home#restricted_index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
